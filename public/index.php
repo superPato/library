dd7e794ef03646ea0003e2eff1a9bb40880c89c5
Cesar Acual <chechaacual@gmail.com>
2018-11-23 19:19:49 -0600

    MySQL-Create

--- public/index.php
+++ public/index.php
@@ -1,13 +1,25 @@
 <?php  
 
 try {
-	$pdo = new PDO('mysql:host=localhost;dbname=librar;charset=utf8', 'library', 'library');
+	$pdo = new PDO('mysql:host=localhost;dbname=library;charset=utf8', 'library', 'library');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$output = "Database connection established";
+
+	$sql = "CREATE TABLE authors (
+		id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY kEY,
+		firstname VARCHAR(200),
+		lastname VARCHAR(200),
+		version INTEGER NOT NULL
+	) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB";
+
+	$pdo->exec($sql);
+
+	$output = "authos table successfully created.";
 } catch (PDOException $e) {
-	$output = "Unable to connect to the database server: " .
-		$e->getMessage() . ' in ' .
-		$e->getFile() . ':' . $e->getLine();
+	$output = sprintf("Database error: %s in %s:%x",
+		$e->getMessage(),
+		$e->getFile(),
+		$e->getLine()
+	);
 }
 
 include __DIR__ . '/../templates/output.html.php';
