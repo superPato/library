341645936170a432fab76d9132c5b185ac5d0008
Cesar Acual <chechaacual@gmail.com>
2018-11-23 20:24:17 -0600

    ListJokes-Shorthand

--- templates/output.html.php
+++ templates/output.html.php
@@ -6,11 +6,15 @@
 	</head>
 	<body>
 		<?php if (isset($error)): ?>
-		<p><?php echo $error ?></p>	
+		<p><?= $error ?></p>	
 		<?php else: ?>
 			<?php foreach ($authors as $author): ?>
 				<blockquote>
-					<p><?php echo $author['lastname'] . ', ' . $author['firstname'] ?></p>
+					<p>
+						<?= htmlspecialchars($author['lastname'], ENT_QUOTES, 'UTF-8') . 
+							', ' . 
+							htmlspecialchars($author['firstname'], ENT_QUOTES, 'UTF-8') ?>
+					</p>
 				</blockquote>
 			<?php endforeach ?>
 		<?php endif ?>
