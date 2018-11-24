b03ac7359aaa89e1448dbc23c9774bd14b834bd9
Cesar Acual <chechaacual@gmail.com>
2018-11-23 19:23:35 -0600

    MySQL-Update

--- public/index.php
+++ public/index.php
@@ -4,16 +4,11 @@ try {
 	$pdo = new PDO('mysql:host=localhost;dbname=library;charset=utf8', 'library', 'library');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$sql = "CREATE TABLE authors (
-		id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY kEY,
-		firstname VARCHAR(200),
-		lastname VARCHAR(200),
-		version INTEGER NOT NULL
-	) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB";
+	$sql = "UPDATE authors SET lastname='Pérez' WHERE firstname='Miguel'";
 
-	$pdo->exec($sql);
+	$affectedRows = $pdo->exec($sql);
 
-	$output = "authos table successfully created.";
+	$output = "Updated " . $affectedRows . " rows.";
 } catch (PDOException $e) {
 	$output = sprintf("Database error: %s in %s:%x",
 		$e->getMessage(),
