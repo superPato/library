95bcfbf1c373c94f857930e1fcfe2de3bb6cc826
Cesar Acual <chechaacual@gmail.com>
2018-11-28 23:19:40 -0600

    ListAuthors-Layout-3

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = 'Internet Library Database';
+
+ob_start();
+
+include __DIR__ . '/../templates/home.html.php';
+
+$output = ob_get_clean();
+
+include __DIR__ . '/../templates/layout.html.php';
---
+++ templates/home.html.php
@@ -0,0 +1,3 @@
+<h2>Internet Library Database</h2>
+
+<p>Welcome to the Internet Library Database</p>
