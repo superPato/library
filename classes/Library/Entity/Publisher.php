d308f9bbd91ac121a802a88aaa37dbc76ed5e034
Cesar Acual <chechaacual@gmail.com>
2019-08-15 10:48:04 -0600

    Relationships-Publisher

---
+++ classes/Library/Entity/Publisher.php
@@ -0,0 +1,26 @@
+<?php 
+namespace Library\Entity;
+
+class Publisher {
+	public $name;
+	public $phone;
+	public $email;
+	private $booksTable;
+
+	public function __construct(\Framework\DatabaseTable $booksTable)
+	{
+		$this->booksTable = $booksTable;
+	}
+
+	public function getBooks()
+	{
+		return $this->booksTable->find('publisherid', $this->id);
+	}
+
+	public function addJoke($book)
+	{
+		$book['publisherid'] = $this->id;
+
+		$this->booksTable->save($book);
+	}
+}
